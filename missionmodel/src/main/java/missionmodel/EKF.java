0680284ebdcb47e6e841accb11b1690be987a411
s-keesing <senna.keesing@gmail.com>
2025-05-04 17:04:44 -0700

    sk-gnc-adcs: adding EKFState and EKF class definitions (skeleton)

---
+++ missionmodel/src/main/java/missionmodel/EKF.java
@@ -0,0 +1,67 @@
+package missionmodel;
+
+public class EKF {
+    private EKFState state;
+    private RealMatrix Q; // Process noise (13x13)
+    private RealMatrix R; // Measurement noise (usually 6x6: position + attitude)
+
+    public EKF(EKFState initialState, RealMatrix processNoise, RealMatrix measurementNoise) {
+        this.state = initialState;
+        this.Q = processNoise;
+        this.R = measurementNoise;
+    }
+
+    public void propagate(Vector3D accMeas, Vector3D gyroMeas, double dt) {
+        // Bias-corrected angular velocity
+        Vector3D omega = gyroMeas.subtract(state.gyroBias);
+        
+        // Quaternion propagation
+        Quaternion dq = QuaternionUtils.integrateOmega(state.attitude, omega, dt);
+        state.attitude = dq.normalize();
+
+        // Velocity and position propagation
+        Vector3D gravity = new Vector3D(0, 0, -1.625);
+        Vector3D accWorld = state.attitude.rotate(accMeas);
+        state.velocity = state.velocity.add(accWorld.add(gravity).scalarMultiply(dt));
+        state.position = state.position.add(state.velocity.scalarMultiply(dt));
+
+        // TODO: Construct F matrix (13x13), propagate covariance
+    }
+
+    public void update(Vector3D posMeas, Quaternion attMeas) {
+        // Measurement residual
+        Vector3D posResidual = posMeas.subtract(state.position);
+        Quaternion qError = attMeas.multiply(state.attitude.inverse());
+        Vector3D deltaTheta = QuaternionUtils.smallAngleApprox(qError);
+
+        // Measurement model Jacobian H (6x13)
+        RealMatrix H = MatrixUtils.createRealMatrix(6, 13);
+        H.setSubMatrix(MatrixUtils.createRealIdentityMatrix(3).getData(), 0, 0); // Position
+        H.setSubMatrix(MatrixUtils.createRealIdentityMatrix(3).getData(), 3, 6); // Attitude error
+
+        // Kalman gain
+        RealMatrix S = H.multiply(state.covariance).multiply(H.transpose()).add(R);
+        RealMatrix K = state.covariance.multiply(H.transpose()).multiply(new LUDecomposition(S).getSolver().getInverse());
+
+        // Apply correction
+        RealVector innovation = new ArrayRealVector(6);
+        innovation.setSubVector(0, posResidual);
+        innovation.setSubVector(3, deltaTheta);
+        RealVector deltaX = K.operate(innovation);
+
+        // Apply to state
+        state.position = state.position.add(deltaX.getSubVector(0, 3));
+        state.velocity = state.velocity.add(deltaX.getSubVector(3, 3));
+        Quaternion dQuat = QuaternionUtils.deltaThetaToQuat(deltaX.getSubVector(6, 3));
+        state.attitude = dQuat.multiply(state.attitude).normalize();
+        state.gyroBias = state.gyroBias.add(deltaX.getSubVector(9, 3));
+
+        // Covariance update
+        RealMatrix I = MatrixUtils.createRealIdentityMatrix(13);
+        state.covariance = I.subtract(K.multiply(H)).multiply(state.covariance);
+    }
+
+    public EKFState getState() {
+        return state;
+    }
+}
---
+++ missionmodel/src/main/java/missionmodel/EKFState.java
@@ -0,0 +1,17 @@
+package missionmodel;
+
+public class EKFState {
+    public Vector3D position;
+    public Vector3D velocity;
+    public Quaternion attitude;
+    public Vector3D gyroBias;
+    public RealMatrix covariance; // 13x13
+
+    public EKFState(Vector3D position, Vector3D velocity, Quaternion attitude, Vector3D gyroBias, RealMatrix covariance) {
+        this.position = position;
+        this.velocity = velocity;
+        this.attitude = attitude;
+        this.gyroBias = gyroBias;
+        this.covariance = covariance;
+    }
+}
