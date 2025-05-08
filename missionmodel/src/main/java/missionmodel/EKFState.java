5bb58f9048202fa66d124d6f02cbf43605914378
s-keesing <senna.keesing@gmail.com>
2025-05-08 13:40:01 -0700

    sk-gnc-adcs: added quaternion class

--- missionmodel/src/main/java/missionmodel/EKFState.java
+++ missionmodel/src/main/java/missionmodel/EKFState.java
@@ -5,7 +5,7 @@ public class EKFState {
     public Vector3D velocity;
     public Quaternion attitude;
     public Vector3D gyroBias;
-    public RealMatrix covariance; // 13x13
+    public RealMatrix covariance;
 
     public EKFState(Vector3D position, Vector3D velocity, Quaternion attitude, Vector3D gyroBias, RealMatrix covariance) {
         this.position = position;
