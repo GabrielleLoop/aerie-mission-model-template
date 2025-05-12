9ff8e4476d47b3c2d65ea554193be3e838de1ae3
s-keesing <senna.keesing@gmail.com>
2025-05-12 14:16:44 -0700

    sk-gnc-adcs: adding all resources to one file

--- missionmodel/src/main/java/missionmodel/DataModel.java
+++ missionmodel/src/main/java/missionmodel/DataModel.java
@@ -5,18 +5,40 @@ import gov.nasa.jpl.aerie.contrib.streamline.modeling.Registrar;
 import gov.nasa.jpl.aerie.contrib.streamline.modeling.discrete.Discrete;
 import gov.nasa.jpl.aerie.contrib.serialization.mappers.DoubleValueMapper;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static gov.nasa.jpl.aerie.contrib.streamline.core.MutableResource.resource;
 import static gov.nasa.jpl.aerie.contrib.streamline.modeling.discrete.Discrete.discrete;
 
 public class DataModel {
 
-    public MutableResource<Discrete<List<Double>>> AngularMomentum;
+    public MutableResource<Discrete<Vector3D>> AngularMomentum;
+    public MutableResource<Discrete<Vector3D>> Position;
+    public MutableResource<Discrete<Vector3D>> Velocity;
+    public MutableResource<Discrete<Quaternion>> Attitude;
+    public MutableResource<Discrete<Vector3D>> AngularVelocity;
+    public MutableResource<Discrete<Vector3D>> AngularAcceleration;
+    public MutableResource<Discrete<Vector3D>> SurfaceCoordinates;
+    public MutableResource<Discrete<Double>> PointingAccuracy;
+    public MutableResource<Discrete<Vector3D>> SunPosition;
 
     public DataModel(Registrar registrar) {
-        AngularMomentum = resource(discrete(Arrays.asList(0.0, 0.0, 0.0)));
-        registrar.discrete("AngularMomentum", AngularMomentum, new ListOfDoubleValueMapper());
+        AngularMomentum = resource(discrete(Vector3D.zero()));
+        Position = resource(discrete(Vector3D.zero()));
+        Velocity = resource(discrete(Vector3D.zero()));
+        Attitude = resource(discrete(Quaternion.identity()));
+        AngularVelocity = resource(discrete(Vector3D.zero()));
+        AngularAcceleration = resource(discrete(Vector3D.zero()));
+        SurfaceCoordinates = resource(discrete(Vector3D.zero()));
+        PointingAccuracy = resource(discrete(0.0));
+        SunPosition = resource(discrete(Vector3D.zero()));
+
+        registrar.discrete("AngularMomentum", AngularMomentum, new Vector3DMapper());
+        registrar.discrete("Position", Position, new Vector3DMapper());
+        registrar.discrete("Velocity", Velocity, new Vector3DMapper());
+        registrar.discrete("Attitude", Attitude, new QuaternionMapper());
+        registrar.discrete("AngularVelocity", AngularVelocity, new Vector3DMapper());
+        registrar.discrete("AngularAcceleration", AngularAcceleration, new Vector3DMapper());
+        registrar.discrete("SurfaceCoordinates", SurfaceCoordinates, new Vector3DMapper());
+        registrar.discrete("PointingAccuracy", PointingAccuracy, new DoubleValueMapper());
+        registrar.discrete("SunPosition", SunPosition, new Vector3DMapper());
     }
 }
